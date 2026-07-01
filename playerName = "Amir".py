5e94f3f9cdf4c9c4c87b1c101369b75f0f810405
amipai <amithalpai@gmail.com>
2026-07-01 13:34:28 -0700

    save madlibs

---
+++ playerName = "Amir".py
@@ -0,0 +1,12 @@
+playerName = "Amir"
+level = 5
+score = 120
+bonus = 30
+finalScore = score + bonus
+
+print("Player Report:")
+print(f"Name: {playerName}")
+print(f"Level: {level}")
+print(f"Score: {score}")
+print(f"Bonus: {bonus}")
+print(f"Final Score: {finalScore}")
