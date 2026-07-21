4cfdef76f49260e86698254efb5c1494120bb157
amipai <amithalpai@gmail.com>
2026-07-21 09:31:00 -0700

    final project

--- unicorn???/pygame_test/my_pygame2.py
+++ unicorn???/pygame_test/my_pygame2.py
@@ -2,7 +2,6 @@ import pygame, sys
 
 pygame.init()
 WIDTH, HEIGHT = 400, 400
-screen = pygame.display.set_mode((WIDTH,HEIGHT), pygame.RESIZABLE)
 pygame.display.set_caption("CTRL+EN Fullscreenn Toggle")
 fullscreen = False
 
@@ -17,18 +16,15 @@ while True:
                 fullscreen = not fullscreen
                 if fullscreen:
                     screen = pygame.display.set_mode(
+                    
                     (800 , 600),
                     pygame.FULLSCREEN |
                     pygame.SCALED)
                 else:
-                    screen = pygame.display.set_mode((WIDTH , HEIGHT), pygame.RESIZABLE)
+                    screen = pygame.display.set_mode(
+                            (WIDTH , HEIGHT), pygame.RESIZABLE)
     screen.fill((30,30,30))
-    pygame.draw.rect(screen, (0,255,0), (1, 5, 100, 100))
-    pygame.display.flip()
+    pygame.draw.rect(screen, (255, 0, 0), (1, 5, 100, 100))
+    pygame.quit()
+                    
 
-
-pygame.draw.rect(screen, (0,255,0), (1, 5, 7, 9))
-pygame.display.update()
-pygame.time.wait(5000)
-
-pygame.quit()
